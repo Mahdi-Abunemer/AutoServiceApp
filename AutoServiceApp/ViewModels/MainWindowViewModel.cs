bb80ddbcd31c0596f269035ae90dc61faeb05fb9
Mahdi-Abunemer <163647199+Mahdi-Abunemer@users.noreply.github.com>
2026-06-15 19:23:49 +0700

    Refactored MainWindowViewModel classes: The defects were "Indecent Exposure". I refactored that  using "Remove Setters" method.

--- AutoServiceApp/ViewModels/MainWindowViewModel.cs
+++ AutoServiceApp/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,6 @@ namespace AutoServiceApp.ViewModels;
 
 public class MainWindowViewModel
 {
-    public AutoServiceManager Manager { get; set; } = new();
-    public string Title { get; set; } = "Auto Service";
+    public AutoServiceManager Manager { get; } = new();
+    public string Title { get; } = "Auto Service";
 }
