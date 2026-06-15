17f5b386130bb7588cb6aa0812c99be760f0ff11
Mahdi-Abunemer <163647199+Mahdi-Abunemer@users.noreply.github.com>
2026-06-15 19:37:52 +0700

    Refactored NotificationFacade classes: The defects were "Primitive Obsession". I refactored that  using "Replace Type Code with Enum" method.

--- AutoServiceApp/Services/NotificationFacade.cs
+++ AutoServiceApp/Services/NotificationFacade.cs
@@ -11,11 +11,11 @@ public class NotificationFacade
         Email = email;
     }
 
-    public void Notify(string type, string phone, string email, string title, string message)
+    public void Notify(NotificationType type, string phone, string email, string title, string message)
     {
-        if (type == "sms")
+        if (type == NotificationType.Sms)
             Sms.SendSms(phone, message);
-        else if (type == "email")
+        else if (type == NotificationType.Email)
             Email.Send(email, title, message);
         else
         {
@@ -23,4 +23,10 @@ public class NotificationFacade
             Email.Send(email, title, message);
         }
     }
+}
+
+public enum NotificationType
+{
+    Sms,
+    Email
 }
